3ffb8513d44d95364919f3698fde717227fd3363
adolfo <jurosauer@itba.edu.ar>
2024-10-30 17:29:51 -0300

    Initial commit

---
+++ server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -0,0 +1,40 @@
+package ar.edu.itba.pod.server;
+
+import com.hazelcast.config.*;
+import com.hazelcast.core.Hazelcast;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("hz-config Server Starting ...");
+
+        GroupConfig groupConfig = new GroupConfig()
+                .setName("g11")
+                .setPassword("g11-pass");
+
+        Config config = new Config().setGroupConfig(groupConfig);
+
+        // Network Config
+        MulticastConfig multicastConfig = new MulticastConfig();
+        JoinConfig joinConfig = new JoinConfig().setMulticastConfig(multicastConfig);
+        InterfacesConfig interfacesConfig = new InterfacesConfig()
+                .setInterfaces(Collections.singletonList("10.15.*.*"))
+                .setEnabled(true);
+        NetworkConfig networkConfig = new NetworkConfig()
+                .setInterfaces(interfacesConfig)
+                .setJoin(joinConfig);
+        config.setNetworkConfig(networkConfig);
+
+        // Management Center Config
+        ManagementCenterConfig managementCenterConfig = new ManagementCenterConfig()
+                .setUrl("http://localhost:8080/mancenter-3.8.5")
+                .setEnabled(true);
+        config.setManagementCenterConfig(managementCenterConfig);
+        Hazelcast.newHazelcastInstance(config);
+    }
+}
