9a4a48584d26eb581787dbcc347cbdc959b1bcb8
adolfo <jurosauer@itba.edu.ar>
2024-11-07 16:49:28 -0300

    small changes + python scripts

--- client/src/main/java/ar/edu/itba/pod/client/utils/Utils.java
+++ client/src/main/java/ar/edu/itba/pod/client/utils/Utils.java
@@ -10,7 +10,6 @@ import com.hazelcast.core.IMap;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
